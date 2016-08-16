1a65a5c35f1f345743dc6437cdcbcee8b9d93c4a
Eonist <unicornhive@gmail.com>
2016-08-17 01:07:53 +0200

    update

---
+++ src/css/CSSTypes.swift
@@ -0,0 +1,19 @@
+import Foundation
+/**
+ * EXAMPLE: Swift.print(CSS.Layout.Display.rawValue)//display
+ */
+enum CSS:String {
+    enum Layout:String {
+        case Display = "display"
+        case Float = "float"
+        case Inline = "inline"
+        case Block = "block"
+    }
+    enum Font:String {
+        case EMS = "ems"
+    }
+    /*other*/
+    case DropShadow = "drop-shadow"
+    case Offset = "offset"
+    case Fill = "fill"
+}

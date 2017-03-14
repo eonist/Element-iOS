a1c9ee057be8da6c19ab42a6e1a701280f2e3fdf
Eonist <30n1st@gmail.com>
2017-03-14 13:52:12 +0100

    Files modified: 1
    Modified 1 file:Explorer-iOS/ICustomButton.swift

--- Explorer-iOS/ICustomButton.swift
+++ Explorer-iOS/ICustomButton.swift
@@ -1,5 +1,16 @@
 import Foundation
 
-class ICustomButton {
+extension CustomButton{
+    override func setChecked(_ checked:Bool) {
+        (self as Custom).setChecked(checked)
+    }
+}
+extension CustomButton:Custom{}
 
+protocol Custom{}
+extension Custom {
+    func setChecked(_ checked:Bool){
+        Swift.print("custom setCheck: \(checked)")
+        (self as! Checkable).setChecked(checked)
+    }
 }

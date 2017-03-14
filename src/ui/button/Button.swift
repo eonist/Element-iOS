70ec4b9dd56b33caae974c7e65bbc146a3019232
Eonist <30n1st@gmail.com>
2017-03-14 14:53:19 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -2,8 +2,6 @@ import Foundation
 
 class Button:Element{
     var isFocused:Bool = false
-    var isSelected:Bool = false
-    var isChecked:Bool = false
     var isDisabled:Bool = false
     override init(){
         super.init()

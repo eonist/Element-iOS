6fa7507efb73bb7a3836a7973f44c053345b4c1d
Eonist <30n1st@gmail.com>
2017-03-14 14:26:42 +0100

    Files modified: 1
    Modified 1 file:src/misc/abstract/Selectable.swift

--- src/misc/abstract/Selectable.swift
+++ src/misc/abstract/Selectable.swift
@@ -1,10 +1,13 @@
 import Foundation
 
 protocol Selectable:class{
-    var selected:Bool {get set}
+    var isSelected:Bool {get set}
 }
 extension Selectable{
     func setSelected(_ selected:Bool){
-        self.selected = selected
+        self.isSelected = selected
+    }
+    func getSelected()->Bool{
+        return self.isSelected
     }
 }

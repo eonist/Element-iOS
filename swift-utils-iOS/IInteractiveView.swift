fa1b39f95efdfff7176f155506681d387ec0d329
Eonist <30n1st@gmail.com>
2017-03-14 17:37:29 +0100

    Files modified: 1
    Modified 1 file:swift-utils-iOS/IInteractiveView.swift

--- swift-utils-iOS/IInteractiveView.swift
+++ swift-utils-iOS/IInteractiveView.swift
@@ -6,6 +6,6 @@ extension InteractiveView:IInteractiveView{}
 extension InteractiveView{
     func touchUpInside(_ event:TouchEvent){
         Swift.print("InteractiveView.touchUpInside()")
-        (self as Touchable).touchUpInside(event)//forward to default Touchable implementation
+        //(self as Touchable).touchUpInside(event)//forward to default Touchable implementation
     }
 }

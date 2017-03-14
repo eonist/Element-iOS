d3e27968f2e59c25df59b366945e627601d6d1d2
Eonist <30n1st@gmail.com>
2017-03-14 17:58:11 +0100

    Files modified: 1
    Modified 1 file:src/IElement.swift

--- src/IElement.swift
+++ src/IElement.swift
@@ -3,8 +3,9 @@ import Foundation
 protocol IElement:IInteractiveView{}
 
 extension Element:IElement{
-    var size:CGSize{get set}
-    var id:String{get set}
-    var parent:NSView{get set}
-    
+    //var size:CGSize{get set}
+    //var id:String{get set}
+    //var parent:NSView{get set}
+    //var skin:ISKin{}
 }
+get 

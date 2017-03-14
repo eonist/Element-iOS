cf1cc4486fc56b1b334ccc7537ad2d24c0e400de
Eonist <30n1st@gmail.com>
2017-03-14 13:29:43 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:src/ui/button/Button.swift

--- src/ui/button/Button.swift
+++ src/ui/button/Button.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class Button {
-
+class Button:Element,IButton{
+    var isFocused:Bool = false
+    var isSelected:Bool = false
+    var isChecked:Bool = false
+    var isDisabled:Bool = false
+    init(){
+        
+    }
 }

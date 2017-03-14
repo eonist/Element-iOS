8b583e5f29e29cb31131438e09fe5d7915eb0b8c
Eonist <30n1st@gmail.com>
2017-03-14 16:01:34 +0100

    Files modified: 1
    Modified 1 file:src/misc/abstract/Touchable.swift

--- src/misc/abstract/Touchable.swift
+++ src/misc/abstract/Touchable.swift
@@ -6,6 +6,6 @@ protocol Touchable:class {
 }
 extension Touchable{
     func touchDown(_ event:TouchEvent){
-        Swift.print("Touchable.touchDown()")
+        Swift.print("Touchable.touchDown() inform parent that touchUp occured")
     }
 }

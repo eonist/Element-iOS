b4379d449d81bddc48c87a2001e29f76dda815c7
Eonist <30n1st@gmail.com>
2017-03-14 14:57:20 +0100

    Files modified: 1
    Modified 1 file:src/ui/button/select/ISelectableButton.swift

--- src/ui/button/select/ISelectableButton.swift
+++ src/ui/button/select/ISelectableButton.swift
@@ -2,4 +2,14 @@ import Foundation
 
 class ISelectableButton {
 
+}/*
+extension SelectButton{
+    /*Selectable*/
+    func setSelected(_ selected:Bool){
+        (self as Selectable).isSelected = selected
+    }
+    func getSelected()->Bool{
+        return (self as Selectable).isSelected
+    }
 }
+*/

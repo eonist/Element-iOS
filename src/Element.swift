d7de2d67f6ec42c03eda3c2720ba85a0f963ec28
Eonist <30n1st@gmail.com>
2017-03-14 14:43:11 +0100

    Files modified: 1
    Modified 1 file:src/Element.swift

--- src/Element.swift
+++ src/Element.swift
@@ -3,7 +3,7 @@ import Foundation
  * NOTE: One tiny limitation is that you cant call super.someMethod() in a sub class. it will still call subType override
  * NOTE: But protocol based UI should encourage calling methods across as oppose to down/up stream. You can always reach the variable the method is manipulating anyway
  * NOTE: Why do we create override proxy methods? to reach protocols. 
- * NOTE: 
+ * NOTE: Override adds bulk, but consider this. a CustomCheckbutton only needs to override 2 methods, not the other 6
  */
 class Element:NSObject {
     override init(){

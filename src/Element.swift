a8abe34800c968eac07037ec6ded62b6c6701d75
Eonist <30n1st@gmail.com>
2017-03-14 14:32:47 +0100

    Files modified: 1
    Modified 1 file:src/Element.swift

--- src/Element.swift
+++ src/Element.swift
@@ -1,7 +1,8 @@
 import Foundation
 /**
- * One tiny limitation is that you cant call super.someMethod() in a sub class. it will still call subType override
- * But protocol based UI should encourage calling methods across as oppose to down/up stream. You can always reach the variable the method is manipulating anyway
+ * NOTE: One tiny limitation is that you cant call super.someMethod() in a sub class. it will still call subType override
+ * NOTE: But protocol based UI should encourage calling methods across as oppose to down/up stream. You can always reach the variable the method is manipulating anyway
+ * NOTE: Why do we create override proxy methods?
  */
 class Element:NSObject {
     override init(){

2b96fc6f0d1775daf2e8e59ea0fd9316f3f00438
Eonist <30n1st@gmail.com>
2017-03-14 15:01:23 +0100

    Files modified: 2
    Modified 2 files:Explorer-iOS/CustomCheckButton.swiftExplorer-iOS/ICustomCheckButton.swift

--- Explorer-iOS/CustomCheckButton.swift
+++ Explorer-iOS/CustomCheckButton.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class CustomCheckButton {
+class CustomCheckButton:CheckButton {
 
 }
--- Explorer-iOS/ICustomCheckButton.swift
+++ Explorer-iOS/ICustomCheckButton.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-class ICustomCheckButton {
+protocol ICustomCheckButton {
 
 }

2f83aa3c93b53b5bc9163202174e99e866255f34
Eonist <30n1st@gmail.com>
2017-03-14 16:07:51 +0100

    Files modified: 1
    Modified 1 file:swift-utils-iOS/IInteractiveView.swift

--- swift-utils-iOS/IInteractiveView.swift
+++ swift-utils-iOS/IInteractiveView.swift
@@ -1,5 +1,9 @@
 import Foundation
 
-class IInteractiveView {
-
+extension InteractiveView:IInteractiveView{}
+protocol IInteractiveView:Touchable{}
+extension IInteractiveView{
+    func touchDown(_ event:TouchEvent){
+        (self as Touchable).touchDown(event)//forward to default Touchable implementation
+    }
 }

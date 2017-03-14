0555667245569639a0d7db826c1bdab5b122e4b5
Eonist <30n1st@gmail.com>
2017-03-14 18:08:18 +0100

    Files modified: 1
    Modified 1 file:src/IElement.swift

--- src/IElement.swift
+++ src/IElement.swift
@@ -1,13 +1,18 @@
 import Foundation
 
-protocol IElement:IInteractiveView{}
-
-extension Element:IElement{
+//Element must implement these
+protocol IElement:IInteractiveView{
     //var size:CGSize{get set}
     //var id:String{get set}
     //var parent:NSView{get set}
     //var skin:ISKin{}//we store state in skin
-    //func getState()
-    //func setState()
+}
+extension Element:IElement{}
+
+//these are overridable from extensions
+extension Element{
+    //func getState()->State
+    //func setState(_ state:State)
     //var classType:String{get}
+    //func resolveSkin()
 }
